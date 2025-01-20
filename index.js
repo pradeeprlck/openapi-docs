0e7a8bafbe463e0419dca346fbabdaaa954b9bc4
Pradeep Reddy <pradeep.chada@rollick.io>
2025-01-20 15:39:25 +0530

    cloud apply response

--- index.js
+++ index.js
@@ -2,38 +2,6 @@ const { generateSpec } = require("har-to-openapi")
 const har = require("./api-list.json");
 const fs = require("fs");
 
-// read a har file from wherever you want - in this example its just a root json object
-
-// const har = {
-//   log: {
-//     entries: [
-//       {
-//         index: 0,
-//         request: {
-//           method: "CUSTOM",
-//           url: "http://test.loadimpact.com/login",
-//           headers: [
-//             {
-//               name: "Content-Type",
-//               value: "application/x-www-form-urlencoded",
-//             },
-//           ],
-//           postData: {
-//             mimeType: "application/x-www-form-urlencoded",
-//             text: "foo0=bar0&foo1=bar1",
-//             params: [
-//               {
-//                 name: "foo0",
-//                 value: "bar0",
-//               },
-//             ],
-//           },
-//         },
-//       },
-//     ],
-//   },
-// };
-
 const convertToOpenAPI = async () => {
     try {
 console.log("har", har);
