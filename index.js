652a29bc1320d05c249ce1c32a36ebfdafe5cef8
Pradeep Reddy <pradeep.chada@rollick.io>
2025-01-20 13:51:24 +0530

    force all in one doc

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ const fs = require("fs");
 const convertToOpenAPI = async () => {
     try {
 console.log("har", har);
-const openapi = await generateSpec(har, { relaxedMethods: true });
+const openapi = await generateSpec(har, { relaxedMethods: true, relaxedContentTypeJsonParse: true, forceAllRequestsInSameSpec: true });
         const { spec, yamlSpec } = openapi;
         console.log("spec", spec);
         fs.writeFileSync("openapi.json", JSON.stringify(spec, null, 2));
