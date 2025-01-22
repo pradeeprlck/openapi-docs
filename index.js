3bbcbc4b84fc8af47009413db07935ed21fdbc3a
Pradeep Reddy <pradeep.chada@rollick.io>
2025-01-22 12:39:23 +0530

    DR APIS

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { generateSpec } = require("har-to-openapi")
-const har = require("./api-list.json");
+const har = require("./api-list-dr.json");
 const fs = require("fs");
 
 const convertToOpenAPI = async () => {
@@ -8,8 +8,8 @@ console.log("har", har);
 const openapi = await generateSpec(har, { relaxedMethods: true, relaxedContentTypeJsonParse: true, forceAllRequestsInSameSpec: true });
         const { spec, yamlSpec } = openapi;
         console.log("spec", spec);
-        fs.writeFileSync("openapi.json", JSON.stringify(spec, null, 2));
-        fs.writeFileSync("openapi.yaml", yamlSpec);
+        fs.writeFileSync("openapi-dr.json", JSON.stringify(spec, null, 2));
+        fs.writeFileSync("openapi-dr.yaml", yamlSpec);
     } catch (error) {
         console.error("Error converting HAR to OpenAPI", error);
     }
